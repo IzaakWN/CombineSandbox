8bb8e63e5348b5338da620c9d9c0533775e17d6e
IzaakWN <izaak.neutelings@cern.ch>
2023-09-28 13:38:32 +0200

    add more

--- README.md
+++ README.md
@@ -3,10 +3,12 @@
 For testing some development of combine.
 
 ## Installation
-Get combine by follow the [instructions for developper](https://cms-analysis.github.io/HiggsAnalysis-CombinedLimit/#for-developers).
+Get combine by follow the [instructions for developers](https://cms-analysis.github.io/HiggsAnalysis-CombinedLimit/#for-developers).
 
 At the time of writing, get CMSSW:
 ```
+source $VO_CMS_SW_DIR/cmsset_default.sh
+export SCRAM_ARCH=slc7_amd64_gcc900
 cmsrel CMSSW_11_3_4
 cd CMSSW_11_3_4/src
 cmsenv
@@ -15,7 +17,7 @@ Install latest combine:
 ```
 git clone git@github.com:cms-analysis/HiggsAnalysis-CombinedLimit.git HiggsAnalysis/CombinedLimit
 ```
-or get my development, e.g.
+_or_, get my development, e.g.
 ```
 git clone git@github.com:IzaakWN/HiggsAnalysis-CombinedLimit.git HiggsAnalysis/CombinedLimit
 git checkout addPOsFromJSON
@@ -27,17 +29,32 @@ bash <(curl -s https://raw.githubusercontent.com/cms-analysis/CombineHarvester/m
 ```
 Install this repository:
 ```
-git clone git@github.com:IzaakWN/HiggsAnalysis-CombinedLimit.git HiggsAnalysis/CombineSandbox
+git clone git@github.com:IzaakWN/CombineSandbox.git HiggsAnalysis/CombineSandbox
+```
+Finally, compile
+```
+scramv1 b clean; scramv1 b -j10
 ```
 
 ## Scripts
 The `generateToyDatacards.py` script creates a simple toy analysis with multiple signal to test the multi-signal physics model.
 1. It creates a ROOT file with input histograms for datacards.
-2. It creates a simple data file `output/datacard.txt` with `CombineHarvester`.
+2. It creates a simple datacard file `output/datacard.txt` with `CombineHarvester`.
+2. It creates a workspace from the datacard file with the `MultiSignalModel` physics model.
+2. It fits the workspace with the `MultiDimFit` method.
 ```
 ./generateToyDatacards.py
 ```
-Now create a workspace with
+
+You can create the workspace yourself with
 ```
 text2workspace.py -m 125 output/datacard.txt -o output/workspace.root -P HiggsAnalysis.CombinedLimit.PhysicsModel:multiSignalModel --PO verbose --PO json=physOpts.json
 ```
+and fit with
+```
+combineTool.py -M MultiDimFit -d output/workspace.root --setParameters r_sig1=1,r_sig2=1,r_sig3=1 --redefineSignalPOIs r_sig1,r_sig2,r_sig3
+```
+
+<p align="center" vertical-align: middle>
+  <img src="../docs/hists.png" alt="Toy analysis" width="450" hspace="20"/>
+</p>
--- generateToyDatacards.py
+++ generateToyDatacards.py
@@ -1,5 +1,7 @@
 #! /usr/bin/env python3
 # Author: Izaak Neutelings (September 2023)
+# Inspiration: https://kcormi.github.io/HiggsAnalysis-CombinedLimit/tutorial2023_unfolding/unfolding_exercise/
+import os
 import ROOT; ROOT.PyConfig.IgnoreCommandLineOptions = True
 from ROOT import gRandom, gROOT, gStyle, TFile, TH1F, TF1, TCanvas, TLegend,\
                  kBlack
@@ -7,7 +9,22 @@ gROOT.SetBatch(True)       # don't open GUI windows
 gStyle.SetOptStat(False)   # don't make stat. box
 gStyle.SetOptFit(1011)     # fit box
 gStyle.SetOptTitle(False)  # don't make title on top of histogram
+gRandom.SetSeed(123456789)
 
+
+def ensuredir(dname,verb=0):
+  """Make directory if it does not exist."""
+  if dname and not os.path.exists(dname):
+    if verb>=1:
+      print(f">>> Making directory {dname}...")
+    os.makedirs(dname)
+  return dname
+  
+
+def joinpath(path,fname):
+  """Join parent directory and filename."""
+  return os.path.join(path,fname) if path else fname
+  
 
 def createhists_sig(nbins,xmin,xmax,nevts=1000,verb=0):
   print(">>> createhists_sig")
@@ -59,9 +76,10 @@ def createhists_obs(hists,verb=0):
   return hobs
   
 
-def plothists(hobs,hists,verb=0):
+def plothists(hobs,hists,outdir="",verb=0):
   """Quick plot of histograms."""
   print(">>> plothists")
+  fname = joinpath(outdir,"hists.png")
   canvas = TCanvas('canvas','canvas',100,100,800,600) # XYWH
   canvas.SetMargin(0.11,0.03,0.11,0.02) # LRBT
   ymax = 0
@@ -90,14 +108,16 @@ def plothists(hobs,hists,verb=0):
     legend.AddEntry(hist,hist.GetTitle(),'l')
   legend.AddEntry(hobs,hobs.GetTitle(),'lep')
   legend.Draw()
-  canvas.SaveAs('hists.png')
+  canvas.SaveAs(fname)
   return canvas
   
 
-def writehists(objs,subdir="cat1",verb=0):
+def writehists(objs,subdir="cat1",outdir="",verb=0):
   """Write histograms to ROOT file."""
   print(f">>> writehists")
-  file = TFile.Open('hists.root','RECREATE')
+  ensuredir(outdir)
+  fname  = joinpath(outdir,"hists.root")
+  file   = TFile.Open(fname,'RECREATE')
   subdir = file.mkdir(subdir)
   subdir.cd()
   for obj in objs:
@@ -105,6 +125,7 @@ def writehists(objs,subdir="cat1",verb=0):
       print(f">>> writehists: Writing {obj.GetName()}...")
     obj.Write(obj.GetName(),obj.kOverwrite)
   file.Close()
+  return fname
   
 
 def createdatacard(finname="hists.root",outdir="output",verb=0):
@@ -128,6 +149,7 @@ def createdatacard(finname="hists.root",outdir="output",verb=0):
   harvester.AddProcesses(   masses, [analysis], eras, chans, procs['sig'], cats, True  )
   harvester.cp().AddSyst(harvester,'lumi','lnN',SystMap()(1.02))
   harvester.cp().AddSyst(harvester,'eff_m','lnN',SystMap()(1.02))
+  harvester.cp().backgrounds().AddSyst(harvester,'norm_bkg','lnN',SystMap()(1.02))
   harvester.cp().ExtractShapes(finname,'$BIN/$PROCESS','$BIN/$PROCESS_$SYSTEMATIC')
   #harvester.cp().backgrounds().ExtractShapes(finname,'$BIN/$PROCESS','$BIN/$PROCESS_$SYSTEMATIC')
   #harvester.cp().signals().ExtractShapes(finname,'$BIN/$PROCESS$MASS','$BIN/$PROCESS$MASS_$SYSTEMATIC')
@@ -137,6 +159,7 @@ def createdatacard(finname="hists.root",outdir="output",verb=0):
   if verb>=1:
     harvester.PrintObs().PrintProcs().PrintSysts()
   ###harvester.AddDatacardLineAtEnd("* autoMCStats %d 0 1"%(threshold)) # [channel] autoMCStats [threshold] [include-signal = 0] [hist-mode = 1]
+  dcname = joinpath(outdir,"datacard.txt")
   writer = CardWriter("$TAG/datacard.txt","$TAG/datacard.input.root") #"$ANALYSIS_$CHANNEL_$BINID_$ERA-$MASS.txt"
   writer.CreateDirectories(True).SetVerbosity(verb)
   writer.WriteCards(outdir,harvester)
@@ -144,19 +167,46 @@ def createdatacard(finname="hists.root",outdir="output",verb=0):
   ###writer.WriteCards(outdir,harvester.bin())
   ###rootfile = TFile("datacard.root",'RECREATE')
   ###harvester.cp().WriteDatacard("datacard.txt",rootfile)
+  return dcname
+  
+
+def createworkspace(dcname,wsname,outdir="",verb=0):
+  """Make workspace with text2workspace.py."""
+  print(f">>> createworkspace")
+  wsname = joinpath(outdir,wsname)
+  cmd = f"text2workspace.py -m 125 {dcname} -o {wsname} "+\
+         "-P HiggsAnalysis.CombinedLimit.PhysicsModel:multiSignalModel "+\
+         "--PO verbose --PO json=physOpts.json"
+  print(f">>> createworkspace: Executing {cmd!r}...")
+  os.system(cmd)
+  return wsname
+  
+
+def mdfit(wsname,verb=0):
+  """Make workspace with text2workspace.py."""
+  print(f">>> mdfit")
+  cmd = f"combineTool.py -M MultiDimFit -d {wsname} "+\
+        " --setParameters r_sig1=1,r_sig2=1,r_sig3=1 --redefineSignalPOIs r_sig1,r_sig2,r_sig3"
+  print(f">>> createworkspace: Executing {cmd!r}...")
+  os.system(cmd)
+  return wsname
   
 
 def main(args):
-  nevts = 5e3
+  nevts  = 5e3
   nbins, xmin, xmax = 50, 0, 100
-  hists = [ ]
+  indir  = "input"
+  outdir = "output"
+  hists  = [ ]
   hists += createhists_sig(nbins,xmin,xmax,nevts=0.4*nevts,verb=args.verbosity)
   hists += createhists_bkg(nbins,xmin,xmax,nevts=0.6*nevts,verb=args.verbosity)
-  hobs  = createhists_obs(hists,verb=args.verbosity)
-  canvas = plothists(hobs,hists,verb=args.verbosity)
-  writehists([canvas,hobs]+hists,verb=args.verbosity)
+  hobs   = createhists_obs(hists,verb=args.verbosity)
+  canvas = plothists(hobs,hists,outdir=indir,verb=args.verbosity)
+  fname  = writehists([canvas,hobs]+hists,outdir=indir,verb=args.verbosity)
   canvas.Close()
-  createdatacard("hists.root",outdir="output",verb=args.verbosity)
+  dcname = createdatacard(fname,outdir=outdir,verb=args.verbosity)
+  wsname = createworkspace(dcname,"workspace.root",outdir=outdir,verb=args.verbosity)
+  mdfit(wsname,verb=args.verbosity)
   
 
 if __name__ == "__main__":
